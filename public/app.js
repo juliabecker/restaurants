28e28a7b38164a5f8ab206ebd3c49baa3a1baa2c
Julia Becker <jcbecker26@gmail.com>
2015-05-02 22:10:58 -0400

    updates

--- public/app.js
+++ public/app.js
@@ -1,14 +1,46 @@
 console.log("Main Linked");
 
+// Establish Global Variables
 var restTemplate = $('script[data-id="restaurant-template"]').text();
+var restDetailTemplate = $('script[data-attr="restaurant-detail-template').text();
+var restFormTemplate = $('script[data-attr="restaurant-form"]').text();
+var listTemplate = $('script[data-id="list-template"]').text();
 
-$('main').on('click', function() {
+// Set Up Routes
+var routes = {
+    "/": showRestaurants,
+    "/restaurant/:restaurant_id": showRestaurant,
+    "/about": showAbout
+}
 
+// Event Listeners
+$('main').on('click', '[data-action="add-restaurant"]', function(e) {
+    $('main').append(restFormTemplate);
 });
 
-getRestaurants();
+$('main').on('click', '[data-action="restaurant-post"]', function(e) {
+    var $form = $(this).parents('div[data-attr="restaurant-post-form"]');
+    var nameInput = $form.find('[data-attr="restaurant-name"]').val();
+    var locationInput = $form.find('[data-attr="restaurant-location"]').val();
+    var cuisineInput = $form.find('[data-attr="restaurant-cuisine"]').val();
+    var imageInput = $form.find('[data-attr="restaurant-image"]').val();
 
-function getRestaurants() {
+    var restaurantObj = {
+        name: nameInput,
+        location: locationInput,
+        cuisine: cuisineInput,
+        image_url: imageInput
+    };
+
+    postRestaurant(restaurantObj)
+    showRestaurants();
+
+})
+
+
+
+function showRestaurants() {
+    emptyMain();
 
     $.ajax({
         method: "GET",
@@ -17,9 +49,25 @@ function getRestaurants() {
         var restaurantEls = restaurants.map(function(restaurant) {
             return Mustache.render(restTemplate, restaurant);
         })
+        var $button = $("<button data-action='add-restaurant' type='submit' class='ui button'>Add New Restaurant</button>");
 
-        $('div[data-id="restaurant-row').append(restaurantEls);
+        $('div[data-attr="restaurant-row"]').append(restaurantEls);
+        $('div[data-attr="button-row"]').append($button);
+
+
+        // <div class="row">
+        //     <button data-action="add-restaurant" type="submit" class="ui button">Add New Restaurant</button>
+        // <div>
+
     });
+}
+
+// function showRestaurant(id) {
+//     console.log(id);
+// }
+
+function showAbout() {
+
 }
 
 function postRestaurant(restaurantObj) {
@@ -28,7 +76,8 @@ function postRestaurant(restaurantObj) {
         method: "POST",
         data: restaurantObj
     }).done(function(data) {
-        console.log(data);
+        // Hide form
+        $('div[data-attr="restaurant-post-form"]').empty();
     });
 }
 
@@ -37,13 +86,66 @@ function deleteRestauraunt(id) {
         url: "/restaurants/" + id,
         method: "DELETE"
     }).done(function() {
-        console.log("deleted")
+        console.log("restaurant deleted")
     });
 }
 
-var restaurant = {
-    name: "Chez Panisse",
-    location: "Berkeley, CA",
-    cuisine: "New American",
-    image_url: "http://nationaleatlocalday.com/wp-content/uploads/2013/09/Chez-Panisse-Laurels-logo-lo-res.jpg"
+function showRestaurant(restaurant_id) {
+
+    emptyMain();
+
+    $.ajax({
+        url: "/restaurants/" + restaurant_id,
+        method: "GET"
+    }).done(function(restaurant) {
+        $('div[data-attr="restaurant-detail"]').append(Mustache.render(restDetailTemplate, restaurant));
+    });
+
+
+
+    $.ajax({
+        url: "/items?restaurant_id=" + restaurant_id,
+        method: "GET"
+    }).done(function(items) {
+        var itemEls = items.map(function(item) {
+            return Mustache.render(listTemplate, item);
+        })
+
+        $('div[data-attr="list-area"]').append(itemEls);
+    });
 }
+
+function postItem(itemObj) {
+    $.ajax({
+        url: "/items",
+        method: "POST",
+        data: itemObj
+    }).done(function() {
+        console.log("item added")
+    });
+}
+
+function emptyMain() {
+    $('div[data-attr="restaurant-row"]').empty();
+    $('div[data-attr="restaurant-detail"]').empty();
+    $('div[data-attr="button-row"]').empty();
+    $('div[data-attr="list-area"]').empty();
+    $('div[data-attr="restaurant-form"]').empty();
+
+}
+
+var item = {
+    restaurant_id: 3,
+    name: "Plum Galette",
+    price: 14,
+    order_count: 0,
+    image_url: "http://cafefernando.com/images/plumgalette.jpg"
+}
+
+
+var router = Router(routes);
+
+// router.init();
+router.init('/');
+
+// showRestaurants();
